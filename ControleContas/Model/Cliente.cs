2893ef59c802f76dadd278814ec59354f9eaab41
lvksounds <lvksounds@gmail.com>
2023-03-15 20:24:31 -0300

    criação da classe clientes

---
+++ ControleContas/Model/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleContas.Model
+{
+    public class Cliente
+    {
+        public string Nome { get; private set; }
+        public string Cpf { get; private set; }
+        public int AnoNascimento { get; private set; }
+        public string IdConta { get; private set; } 
+
+        public Cliente(string nome, string cpf, int anoNascimento)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            AnoNascimento = anoNascimento;
+        }
+
+
+        public Cliente()
+        {
+
+        }
+    }
+}
--- ControleContas/Program.cs
+++ ControleContas/Program.cs
@@ -1,6 +1,24 @@
 ﻿
 using ControleContas.Model;
 
-Conta conta = new Conta("1234");
-Conta conta2 = new Conta("4321", 1000.99m);
-Console.WriteLine($"Conta: {conta.Numero}; Saldo: {conta.Saldo}");
+
+var contas = new List<Conta>
+{
+   new Conta("1234", 1244.34m),
+   new Conta("654321", 2341.42m)
+};
+contas.ForEach(conta => Console.WriteLine(conta.Saldo));
+
+Console.WriteLine($"Maior saldo: {contas.Max(conta => conta.Saldo)}");
+
+Console.WriteLine($"Total geral de saldo das contas: {contas.Sum(conta => conta.Saldo)}");
+
+var clientes = new List<Cliente>
+{
+    new Cliente("Lucas", "17304026740", 100698),
+    new Cliente("Julia", "16095380723", 30101999)
+};
+
+clientes.ForEach(cliente => contas.ForEach(conta => cliente.IdConta = conta.Numero));
+
+clientes.ForEach(cliente => Console.WriteLine(cliente.IdConta));
